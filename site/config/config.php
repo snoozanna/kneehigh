84f4d8d942d3b68dc68a019cda167d533c78fc0b
snoozanna <hurstsuzanna@gmail.com>
2026-09-02 22:43:49 +0100

    panel again

--- site/config/config.php
+++ site/config/config.php
@@ -10,7 +10,7 @@
 return [
     'debug' => true,
     'yaml.handler' => 'symfony', // already makes use of the more modern Symfony YAML parser: https://getkirby.com/docs/reference/system/options/yaml (will become the default in a future Kirby version), 
-//     'panel' =>[
-//     'install' => true
-//   ]
+    'panel' =>[
+    'install' => true
+  ]
 ];
