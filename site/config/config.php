af6d954fa54bfd8f59ac2212cf1d653642e71aff
snoozanna <hurstsuzanna@gmail.com>
2026-09-18 17:51:48 +0100

    change config

--- site/config/config.php
+++ site/config/config.php
@@ -3,15 +3,14 @@
 /**
  * The config file is optional. It accepts a return array with config options
  * Note: Never include more than one return statement, all options go within this single return array
- * In this example, we set debugging to true, so that errors are displayed onscreen. 
- * This setting must be set to false in production.
+ * Keep debugging disabled in production so errors are not displayed onscreen.
  * All config options: https://getkirby.com/docs/reference/system/options
  */
 return [
-    'debug' => true,
+    'debug' => false,
     'yaml.handler' => 'symfony', // already makes use of the more modern Symfony YAML parser: https://getkirby.com/docs/reference/system/options/yaml (will become the default in a future Kirby version), 
-    'panel' =>[
-    'install' => true
+    'panel' => [
+    'install' => false
   ],
   // 'cache' => [
   //   'pages' => [
