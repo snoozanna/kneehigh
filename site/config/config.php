7aa682dee65fd459505bc4cf7a5daf33d58bc079
snoozanna <hurstsuzanna@gmail.com>
2026-09-19 11:51:46 +0100

    panel install

--- site/config/config.php
+++ site/config/config.php
@@ -11,7 +11,7 @@ return [
     'yaml.handler' => 'symfony', // already makes use of the more modern Symfony YAML parser: https://getkirby.com/docs/reference/system/options/yaml (will become the default in a future Kirby version), 
    'panel' => [
     'theme' => 'light',
-    'install' => false,
+    'install' => true,
 
   ]
   // 'cache' => [
