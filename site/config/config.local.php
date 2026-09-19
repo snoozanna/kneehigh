a9492b6e805fdbec82c8a2ac2b88cc955262ff3c
snoozanna <hurstsuzanna@gmail.com>
2026-09-19 11:45:15 +0100

    try with other config files

--- site/config/config.local.php
+++ site/config/config.local.php
@@ -7,7 +7,7 @@
  * All config options: https://getkirby.com/docs/reference/system/options
  */
 return [
-    'debug' => false,
+    'debug' => true,
     'yaml.handler' => 'symfony', // already makes use of the more modern Symfony YAML parser: https://getkirby.com/docs/reference/system/options/yaml (will become the default in a future Kirby version), 
    'panel' => [
     'theme' => 'light',
